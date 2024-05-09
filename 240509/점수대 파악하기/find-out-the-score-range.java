c74f6032e6bae4eea3818674e050f495129834db
eomao <156084917+eomao@users.noreply.github.com>
2024-05-09 22:52:31 +0900

    Title: 점수대 파악하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240509/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int n;
+
+        while(true){
+            n = sc.nextInt();
+            if(n == 0){
+                break;
+            }
+            if(n/10 != 0){
+                arr[n/10-1]++;
+            }
+        }
+        for(int i = 9; i > -1; i--){
+            System.out.println((i+1)*10+" - "+arr[i]);
+        }
+    }
+}
