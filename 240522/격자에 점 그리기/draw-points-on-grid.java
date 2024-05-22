bed17c74793b9c39cab7ea0cc1f9384cbdc9763b
eomao <156084917+eomao@users.noreply.github.com>
2024-05-22 14:16:38 +0900

    Title: 격자에 점 그리기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240522/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][n];
+        int cnt = 1, a, b;
+
+        for(int i = 0; i < m; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+            arr[a-1][b-1] = cnt;
+            cnt++;
+        }
+
+        
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                System.out.print(arr[r][c]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
