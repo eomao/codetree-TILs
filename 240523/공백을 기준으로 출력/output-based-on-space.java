98f172980f8c8316421efde3160c62883e0be9b4
eomao <156084917+eomao@users.noreply.github.com>
2024-05-23 14:04:03 +0900

    Title: 공백을 기준으로 출력, Time: 105ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240523/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ' '){
+                System.out.print(str.charAt(i));
+            }
+        }
+        for(int i = 0; i < str2.length(); i++){
+            if(str2.charAt(i) != ' '){
+                System.out.print(str2.charAt(i));
+            }
+        }
+
+
+    }
+}
