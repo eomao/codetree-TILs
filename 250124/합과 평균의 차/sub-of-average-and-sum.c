91602ed9460a542cf93cbc279dd70af94783b4ed
eomao <156084917+eomao@users.noreply.github.com>
2025-01-24 15:03:31 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250124/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%d\n%d", a+b+c, (a+b+c)/3, a+b+c- (a+b+c)/3);
+    // Please write your code here.
+    return 0;
+}
