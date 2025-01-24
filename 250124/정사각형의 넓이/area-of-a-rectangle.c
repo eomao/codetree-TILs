9e28d5efc6239ce91f34a2ce3a1ba22e7fcb782c
eomao <156084917+eomao@users.noreply.github.com>
2025-01-24 15:06:44 +0900

    Title: 정사각형의 넓이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250124/정사각형의 넓이/area-of-a-rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n*n);
+    if(n < 5){
+        printf("tiny\n");
+    }
+    // Please write your code here.
+    return 0;
+}
