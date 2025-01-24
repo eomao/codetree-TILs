2fdf33a9e9674a95b3debad144bc5614eaf87126
eomao <156084917+eomao@users.noreply.github.com>
2025-01-24 15:59:06 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250124/삼항연산자 2/ternary-operator-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    if(a == 1){
+        printf("t");
+    }else{
+        printf("f");
+    }
+    // Please write your code here.
+    return 0;
+}
