8d991dfe4443b5222c58977443cbe0ffde299aed
eomao <156084917+eomao@users.noreply.github.com>
2025-01-26 15:14:14 +0900

    Title: 1부터 n까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250126/1부터 n까지 출력/print-from-1-to-n.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        printf("%d ", i);
+    }
+    // Please write your code here.
+    return 0;
+}
