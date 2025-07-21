564eba1e5cc2a74f113458de303aa3cbc7c9a4d5
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 22:22:30 +0900

    Title: 변수 값 복사하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    a = c;
+    b = c;
+    printf("%d %d %d", a, b, c);
+    return 0;
+}
