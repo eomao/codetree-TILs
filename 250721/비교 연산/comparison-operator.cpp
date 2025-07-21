1f561c3d77148be895d9a7b220c6209001afc0be
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 21:39:51 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/비교 연산/comparison-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    // Please write your code here.
+    scanf("%d %d", &A, &B);
+    printf("%d\n", A>=B);
+    printf("%d\n", A>B);
+    printf("%d\n", A<=B);
+    printf("%d\n", A<B);
+    printf("%d\n", A==B);
+    printf("%d\n", A!=B);
+    return 0;
+}
