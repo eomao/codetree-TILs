52c6ae6e938b0006b21e1f2e5466711c1e8d35b3
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 14:11:12 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    float c = (a+b)/2.0;
+    printf("%d %.1f", a+b, c);
+    // Please write your code here.
+    return 0;
+}
