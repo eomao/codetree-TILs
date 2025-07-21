96130710d3a45b0b78ae48ef80ae5ca531396128
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 22:07:50 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i;
+    char c;
+    scanf("%c", &c);
+    for(i = 0; i < 8; i++){
+        printf("%c", c);
+    }
+    return 0;
+}
