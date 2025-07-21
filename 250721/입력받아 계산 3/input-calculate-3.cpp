7d103a3d4a7664f46925b2229f51b11fe2129406
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 14:00:26 +0900

    Title: 입력받아 계산 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/입력받아 계산 3/input-calculate-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    // Please write your code here.
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d", a*b);
+    return 0;
+}
