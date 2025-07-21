e0ca640cf0ab09b6c36de2de1ae00418305bdbc1
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 22:34:43 +0900

    Title: 길이 단위 환산하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float N;
+    scanf("%f", &N);
+    N *= 30.48;
+    printf("%.1f", N);
+    return 0;
+}
