ff3a67e6b2c3d128c0e25841561a4a320a44b45d
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 22:39:04 +0900

    Title: 입력받아 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/입력받아 출력/input-and-output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d %d", a, b);
+    return 0;
+}
