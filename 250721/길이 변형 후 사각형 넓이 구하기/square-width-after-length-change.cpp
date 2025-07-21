8016ca779f70f775cd18f8d2ff40524dee993b4c
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 14:06:18 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    // Please write your code here.
+    scanf("%d %d", &a, &b);
+    a += 8;
+    b *= 3;
+    printf("%d\n%d\n%d", a, b, a*b);
+    return 0;
+}
