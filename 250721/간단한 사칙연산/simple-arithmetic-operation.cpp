292bc529b3a25100846fa7dcedf44c4b8c7437f5
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 14:04:15 +0900

    Title: 간단한 사칙연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    // Please write your code here.
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+    return 0;
+}
