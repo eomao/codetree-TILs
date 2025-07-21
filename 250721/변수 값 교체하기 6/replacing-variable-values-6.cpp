4efb939baaebf06234b7825e55d963593d7365bd
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 22:21:06 +0900

    Title: 변수 값 교체하기 6, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 2;
+    int b = 5;
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("%d\n%d", a, b);
+    return 0;
+}
