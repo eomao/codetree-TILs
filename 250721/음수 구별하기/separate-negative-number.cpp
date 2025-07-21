905d1d42d03240a1200aa01f13864a7849201241
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 21:22:52 +0900

    Title: 음수 구별하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    printf("%d\n", N);
+    if(N < 0){
+        printf("minus");
+    }
+    // Please write your code here.
+    return 0;
+}
