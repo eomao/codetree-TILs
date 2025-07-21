fe2821ab480b6d32ea1055db3d62c6872fd0e1f9
eomao <156084917+eomao@users.noreply.github.com>
2025-07-21 22:05:30 +0900

    Title: 감소시키며 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/감소시키며 출력하기/decrease-and-print.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i;
+    for(i = 17; i > 4; i--){
+        printf("%d ", i);
+    }
+    return 0;
+}
