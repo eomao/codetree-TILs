b85b04e0ff27f48273ec3ea3cee5c48c03c6254b
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 13:44:10 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, i;
+    scanf("%d", &n);
+    for(i = n; i <= 100; i++){
+        printf("%d ",i);
+    }
+    return 0;
+}
