92015816defeed0971c0f41fb9d47540dcee722c
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 12:08:56 +0900

    Title: 시력 검사 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a;
+    scanf("%f", &a);
+    if(a >= 1.0){
+        printf("High");
+    }else if(a >= 0.5){
+        printf("Middle");
+    }else{
+        printf("Low");
+    }
+    return 0;
+}
