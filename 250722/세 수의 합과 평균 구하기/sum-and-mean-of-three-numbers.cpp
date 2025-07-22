31a14feeb6a1fd29997f2d0c0dddb5497c4d43d7
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 11:44:28 +0900

    Title: 세 수의 합과 평균 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%d", a+b+c, (a+b+c)/3);
+    // Please write your code here.
+    return 0;
+}
