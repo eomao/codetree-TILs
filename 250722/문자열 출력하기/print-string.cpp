eb46965ee1047ef6ba7f7bf73d88cc177c08ddfe
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 13:49:34 +0900

    Title: 문자열 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/문자열 출력하기/print-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N, i;
+    scanf("%d", &N);
+    for(i = 0; i < N; i++){
+        printf("LeebrosCode\n");
+    }
+    return 0;
+}
