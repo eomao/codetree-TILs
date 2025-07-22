7fbcf03bcec70adf707d64a5a505a38d603ef353
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 11:20:17 +0900

    Title: 실수와 문자 받아 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a, b;
+    char c;
+    scanf("%c", &c);
+    scanf("%f", &a);
+    scanf("%f", &b);
+    printf("%c\n%.2f\n%.2f", c, a, b);
+    return 0;
+}
