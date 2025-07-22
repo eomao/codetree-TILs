b28ed1f5842cc6b93d7aa40883618f93bcc8f9ac
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 13:48:07 +0900

    Title: 특정 숫자 도달하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N, i;
+    scanf("%d", &N);
+    for(i = N; i > 0; i--){
+        printf("%d ", i);
+    }
+    return 0;
+}
