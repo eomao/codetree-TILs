5408b48bbf5d26540fb49f7c7aa44d1ed78dd2b7
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 13:41:26 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/장학금/scholarship.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int mid, fin;
+    scanf("%d %d", &mid, &fin);
+    if(mid >= 90){
+        if(fin >= 95){
+            printf("100000");
+        }else if(fin >= 90){
+            printf("50000");
+        }else{
+            printf("0");
+        }
+    }else{
+        printf("0");
+    }
+    return 0;
+}
