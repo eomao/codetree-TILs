d1d34ae0f0463a923695fcba3e549f6fed5868be
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 11:38:20 +0900

    Title: 두수의 곱과 몫, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %d", a, b, a/b);
+    return 0;
+}
