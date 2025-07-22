6f253cc153da69133a1d3baa9d291a11c2224364
eomao <156084917+eomao@users.noreply.github.com>
2025-07-22 13:46:30 +0900

    Title: 정수 입력받아 배수 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N, i;
+    scanf("%d", &N);
+    for(i = 1; i <= 5; i++){
+        printf("%d ", N*i);
+    }
+    return 0;
+}
