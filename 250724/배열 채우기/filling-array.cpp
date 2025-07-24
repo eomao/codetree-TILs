cd20f9b664a10b220e75d6797fcf3811d34af6ea
eomao <156084917+eomao@users.noreply.github.com>
2025-07-24 12:00:59 +0900

    Title: 배열 채우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250724/배열 채우기/filling-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[11];
+    int mark = 10, i;
+    for(i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
+        if(arr[i] == 0){
+            mark = i-2;
+            break;
+        }
+    }
+    for(i = mark; i >= 0; i--){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
